956a84157963809e176784fbdaf86723c653106c
Jay Figaro <joey@joeyfigaro.com>
2025-03-21 23:14:44 -0400

    chore: coverage configuration tweaks

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,11 @@ export default defineConfig({
   test: {
     environment: 'happy-dom',
     globals: true,
+    coverage: {
+      reporter: ['text-summary'],
+      exclude: ['node_modules', 'dist', '*.config.*'],
+      provider: 'v8',
+    },
     setupFiles: ['./test/setup.ts'],
     include: ['**/*.spec.{ts,tsx}'],
   },
